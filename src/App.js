968a0888497a24909eab2ff60d4ca7292ff11433
= <=>
2023-02-04 11:26:35 +0300

    add animate

--- src/App.js
+++ src/App.js
@@ -8,23 +8,21 @@ function App() {
     <div className="App">
          <button onClick={()=> setOpen(true)} className="open-modal-btn"> Открыть окно</button>
 
-         {
-          open && (
-            <div className="overlay">
+        
+            <div className={`overlay animated ${open ? 'show' : ''}`}>
               <div className="modal">
-                <button onClick={()=>setOpen(false)}>
-                <svg height="200" viewBox="0 0 200 200" width="200">
+              
+                <svg onClick={()=>setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
                   <title />
                   <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
                 </svg>
                
-                </button>
+                 
                 <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
               </div>
             </div> 
 
-          )
-         }
+          
      
     </div>
     
